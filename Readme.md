125163df556c0ee673179dd01c97db52030b7ffa
mhdwell <augfarel@gmail.com>
2017-07-08 05:40:49 -0500

    ven

---
+++ Readme.md
@@ -0,0 +1,17 @@
+#  Documento base dos
+## Introducion
+el disdw de jsasd
+### caracteristicas
+---
+### sofware 
+1. brackets
+1. windows
+1. markdown
+1. internet
+
+> Es html 
+
+|campo  1 | 222 | wqw|
+|campo  2 | 333 | rew|
+
+typora
---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    $(".button").click (function(){ 
+        $(".frente").toggleClass("volteada");
+		$(".middle").toggleClass("d");
+                                  
+               });
+     $(".buttonIco").click (function(){ 
+       
+        $(".buttonIco").toggleClass("icot");
+        $(".icono").toggleClass("icor");
+         $(".widget").toggleClass("escale");
+         $(".widgetcon").toggleClass("transl");
+
+
+               });
+});
